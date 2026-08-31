4cbd075e79e1338d7e74a35fce19aefff0f8620e
Vinicius Solique <113440728+viniciussolique39@users.noreply.github.com>
2026-08-30 20:51:36 -0400

    Ajustado as rotas
    Colocado a edição de informações em funcionamento.

--- app/api/clientes/[id]/route.js
+++ app/api/clientes/[id]/route.js
@@ -1,45 +1,70 @@
-import db from '@/lib/db';
-import { NextResponse } from 'next/server';
+import db from "@/lib/db";
 
 export async function PUT(request, { params }) {
-  try {
-    const { id } = await params;
-    const { nome, email, telefone } = await request.json();
 
-    if (!nome || !email) {
-      return NextResponse.json(
-        { erro: 'Nome e e-mail são obrigatórios.' },
-        { status: 400 }
-      );
-    }
+  try{
+
+    const {id} = await params;
+    const dados = await request.json();
+
+    const { nome, email, telefone } = dados;
 
     await db.query(
-      `UPDATE clientes
-       SET nome = ${nome}, email = ${email}, telefone = ${telefone}
-       WHERE id = ${id}`,
-      [nome, email, telefone || null, id]
-    );
+       `UPDATE clientes
+       SET nome = $1,
+           email = $2,
+           telefone = $3
+       WHERE id = $4`,
 
-    return NextResponse.json({ mensagem: 'Cliente atualizado com sucesso.' });
+       [nome, email, telefone, id]
+    );
+    return Response.json({
+      mensagem:"Cliente atualizado com sucesso"
+
+    });
+  
   } catch (erro) {
-    return NextResponse.json(
-      { erro: 'Erro ao atualizar cliente', detalhe: erro.message },
+    console.log("ERRO AO ATUALIZAR INFORMAÇÕES");
+    console.log(erro);
+    
+    return Response.json(
+      {
+        erro: "Erro ao atualizar",
+        detalhe: erro.mensssage
+      },
       { status: 500 }
     );
   }
 }
 
-export async function DELETE(request, { params }) {
-  try {
+export async function DELETE(request, {params}) {
+  try{
+
     const { id } = await params;
 
-    await db.query('DELETE FROM clientes WHERE id = $1', [id]);
+    await db.query(
+      "DELETE FROM clientes WHERE id = $1",
+      [id]
+    );
 
-    return NextResponse.json({ mensagem: 'Cliente excluído com sucesso.' });
+    return Response.json({
+
+      mensagem: "Cliente foi excluído com sucesso"
+
+    });
+
   } catch (erro) {
-    return NextResponse.json(
-      { erro: 'Erro ao excluir cliente', detalhe: erro.message },
+    console.log("ERRO AO EXCLUIR:");
+    console.log(erro);
+
+    return Response.json(
+{
+        erro: "Erro ao excluir cliente",
+        detalhe: erro.message
+      },
       { status: 500 }
     );
-  }
+
+}
+
 }
